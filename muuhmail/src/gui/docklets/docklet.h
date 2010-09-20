3d3b4ee6457978a1f5e50027491a97bb49133dee
Christian Kessler <chkessle@googlemail.com>
2010-09-20 14:49:20 +0200

    created new folder for docklets

---
+++ muuhmail/src/gui/docklets/docklet.h
@@ -0,0 +1,23 @@
+#ifndef DOCKLET_H
+#define DOCKLET_H
+
+#include <QWidget>
+#include <QGridLayout>
+#include <QTableWidget>
+
+class Docklet : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Docklet(QWidget *parent = 0);
+
+private:
+    void initialize();
+
+signals:
+
+public slots:
+
+};
+
+#endif // DOCKLET_H
